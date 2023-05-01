1b1569fa224c2fba874074373a2c3d7deb342ab5
hareem7bilal <91612899+hareem7bilal@users.noreply.github.com>
2023-05-01 23:39:04 +0500

    styled pricing plans page

---
+++ client/src/components/plans/proPlan.jsx
@@ -0,0 +1,43 @@
+import "./plans.css";
+import { Grid } from "@material-ui/core";
+
+export default function ProPlan() {
+  return (
+    <Grid item xs={6} sm={6} md={6}>
+      <div className="plan" style={{ marginRight: "none" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <h5>Pro Plan</h5>
+          <div style={{fontSize:"14px"}}>
+          $19 / monthly<br/>
+          $147 / yearly
+          </div>
+          <p></p>
+        </div>
+      
+          <p>Get Access to:</p>
+
+        <ul className="planList"  type="circle">
+          <li>Limited Number of Projects</li>
+          <li>100+ Predesigned Prompts</li>
+          <li>Edit and Customize Prompts</li>
+          <li>Browser Extension</li>
+          <li>UNLIMITED Content Generation</li>
+          <li>Save unlimited copy</li>
+          <li>Access all your copy in one place</li>
+          <li>Create your own prompts</li>
+          <li>Get access to all future updates</li>
+          <li>Priority Email Support</li>
+        </ul>
+        <button type="button" class="btn btn-light">
+          Get Started
+        </button>
+      </div>
+    </Grid>
+  );
+}
---
+++ client/src/pages/pricingplans/pricingplans.jsx
@@ -0,0 +1,25 @@
+import "./pricingplans.css";
+import { Grid } from "@material-ui/core";
+import FreePlan from "../../components/plans/freePlan";
+import ProPlan from "../../components/plans/proPlan";
+
+export default function PricingPlans() {
+  return (
+    <Grid container style={{ justifyContent: "center" }}>
+      <div style={{display:"flex", flexDirection:"column", margin: "50px 0", alignItems: "center"}}  >
+        <h2>
+          <u>Choose a Plan</u>
+        </h2>
+        <div className="periodical">
+          <p className="periodicalItem" style={{borderRight:'none'}}>Monthly</p>
+          <p className="periodicalItem">Annually</p>
+        </div>
+        <p>Pay monthy or yearly. Cancel at any time</p>
+        <Grid item xs={12} sm={12} md={12} style={{display:"flex"}} >
+            <FreePlan/>
+            <ProPlan/>
+        </Grid>
+      </div>
+    </Grid>
+  );
+}
