d027077ec86e7d40be334bf9628aa94cc6763b61
Amina Wasif <aminawasif20@gmail.com>
2023-05-09 09:33:36 +0500

    Modified App.js in client

--- client/src/App.js
+++ client/src/App.js
@@ -8,27 +8,25 @@ import Signup from "./pages/signup-in/signup";
 import Signin from "./pages/signup-in/signin";
 import PricingPlans from "./pages/pricingplans/pricingplans";
 import StripeCheckout from "./pages/stripecheckout/stripecheckout";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./app.css"
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/prompts" element={<PromptLibrary />} />
-        <Route path="/account" element={<AccountInfo />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/pricing" element={<PricingPlans/>}/>
-        <Route path="/checkout" element={<StripeCheckout/>}/>
-
-
-      </Routes>
-    </BrowserRouter>
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/prompts" element={<PromptLibrary />} />
+          <Route path="/account" element={<AccountInfo />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/pricing" element={<PricingPlans/>}/>
+          <Route path="/checkout" element={<StripeCheckout/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
