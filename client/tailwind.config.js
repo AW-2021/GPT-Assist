76b46ea6b65008c545ba501225aee98c846c18a1
Amina Wasif <aminawasif20@gmail.com>
2023-05-09 09:28:49 +0500

    Added in tailwindCSS to the project's client

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   mode: 'jit',
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "480px",
+        sm: "768px",
+        md: "1024px"
+      },
+    },
   },
   plugins: [],
 }
