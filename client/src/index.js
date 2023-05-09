f308fcd90afcd42983cbaa899f0caabc7d67d1bc
Amina Wasif <aminawasif20@gmail.com>
2023-05-09 09:45:20 +0500

    Updated index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { createRoot } from 'react-dom/client';
+import './index.css';
 import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App tab="home" />);
+root.render(
+    <App tab="home" />
+);
