cc1f06b6f386de2999b3cb1a9c65b498ad2fdb2f
Amina Wasif <aminawasif20@gmail.com>
2023-05-09 09:41:41 +0500

    Converted styling in tailwindCSS format & added responsiveness by using grids

--- client/src/components/promptBox/promptBox.jsx
+++ client/src/components/promptBox/promptBox.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import "./promptBox.css";
 
 export default function PromptBox(props) {
   return (
-    <div className="prompt">
-      <div style={{display:'flex'}}>
-        <img className="promptImg" src={props.src} alt={props.alt} />
-        <p className="new">NEW</p>
+    <div className="relative flex flex-col p-3 border-[2px] border-[#086cb9] rounded-[18px] 
+      hover:shadow-xl hover:shadow-sky-600/30">
+      <div className="flex justify-between items-center">
+        <img className="w-[40px] h-[40px] rounded-[50%] bg-[rgb(216,209,209)]" 
+          src={props.src} alt={props.alt} />
+        <p className="bg-[#174b8b] text-white px-3 py-1 text-[10px] rounded-[5px]">NEW</p>
       </div>
 
-      <div className="promptText">{props.txt}</div>
-      <p className="promptDesc">
-        "Write engaging content on how AI is changing the world"
+      <div className="text-[16px] my-2">{props.txt}</div>
+      <p className="text-[13px] pb-3">
+        <i>"{props.desc}"</i>
       </p>
     </div>
   );
