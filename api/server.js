ffa99d05e973748bc8cca593a15ea4a140983adb
Amina Wasif <aminawasif20@gmail.com>
2023-05-12 10:07:38 +0500

    Added new server side router for /prompt to handle requests from promptLibrary page.

--- api/server.js
+++ api/server.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require('cors')
-const checkoutRoute=require("./routes/checkout")
-const chatRoute=require("./routes/chat")
-const authRoute=require("./routes/auth")
+const checkoutRoute = require("./routes/checkout");
+const chatRoute = require("./routes/chat");
+const authRoute = require("./routes/auth");
+const promptRoute = require("./routes/prompt");
 
 require("dotenv").config();
 
@@ -23,13 +24,13 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => {
-    console.log("MongoDB database connection established successfully!");
-  });
+  .then(() => console.log("MongoDB database connection established successfully!"))
+  .catch(err => console.error('Failed to connect to MongoDB', err));
 
 app.use("/checkout",checkoutRoute)
 app.use("/chat",chatRoute)
 app.use("/auth",authRoute)
+app.use('/prompt', promptRoute)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
