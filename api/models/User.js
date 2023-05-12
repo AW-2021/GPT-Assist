8bc03a12140b52725f464a36ac521bb61ed8b1b8
Amina Wasif <aminawasif20@gmail.com>
2023-05-12 10:06:16 +0500

    Updated User.js

--- api/models/User.js
+++ api/models/User.js
@@ -1,32 +1,25 @@
 const mongoose=require("mongoose");
 
-const UserSchema=new mongoose.Schema(
-    {
-        username:{
-            type:String,
-            required:true,
-            min:3,
-            max:20,
-            unique:true,
-        },
-
-        email:{
-            type:String,
-            require:true,
-            max:50,
-            unique:true,
-
-        },
-
-        password:{
-            type:String,
-            require:true,
-            min:8,
-        }
+const UserSchema=new mongoose.Schema({
+    
+    username:{
+        type:String,
+        required:true,
+        min:3,
+        max:20,
+        unique:true,
+    },
+    email:{
+        type:String,
+        require:true,
+        max:50,
+        unique:true,
+    },
+    password:{
+        type:String,
+        require:true,
+        min:8,
     }
-
-       
-);
-
+});
 
 module.exports=mongoose.model("User",UserSchema)
