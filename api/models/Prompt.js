ac80be0709dcbfbc0459df581742c1dd6a6622c5
Amina Wasif <aminawasif20@gmail.com>
2023-05-12 10:08:16 +0500

    Created new MongoDB model for Prompts

---
+++ api/models/Prompt.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const promptSchema = new mongoose.Schema({
+
+    category: { 
+        type: String, 
+        required: true, 
+        enum: ['Social Media', 'YouTube', 'Twitter', 'Blog Articles', 'Copy Writing', 'LinkedIn', 'Others', 'professional','personal', 'question', 'response', 'joke', 'quote', 'fact', 'news', 'event', 'feedback'],
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    userAccessType: {
+        type: String,
+        required: true,
+        enum: ['free', 'premium'],
+        default: 'free',
+    },
+
+});
+
+const Prompt = mongoose.model('Prompt', promptSchema);
+module.exports = Prompt;
